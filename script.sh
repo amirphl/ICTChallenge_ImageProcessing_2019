a2c781fd5d363a5d46883218f26ca274450888fd
amirphl <amirphl@aut.ac.ir>
2019-03-08 11:58:44 +0330

    change file path

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-for i in `seq 1 151`;
-        do
-                python3.5 text_recognition.py -i "sample_images/$i.jpg" -east frozen_east_text_detection.pb
-        done
+#for i in `seq 1 151`;
+#        do
+#                python3.5 text_recognition.py -i "sample_images/$i.jpg" -east frozen_east_text_detection.pb
+#        done
+python3.5 text_recognition.py -i "images/" -east frozen_east_text_detection.pb
